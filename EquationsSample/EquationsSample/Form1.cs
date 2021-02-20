f97f7032099f818abf4dd3ac5daee071ac699504
Vladislav Parkhomenko <vladislav.parkhomenko@yandex.ua>
2021-02-20 19:22:17 +0200

    add validation for equation input

--- EquationsSample/EquationsSample/Form1.cs
+++ EquationsSample/EquationsSample/Form1.cs
@@ -7,6 +7,8 @@ namespace EquationsSample
     {
         private Equation _equation;
 
+        private bool _isInputValid;
+
         public Form1()
         {
             InitializeComponent();
@@ -14,12 +16,29 @@ namespace EquationsSample
 
         private void btEvaluate_Click(object sender, EventArgs e)
         {
-            ReadInput();
-            OutputSolution();
+            ReadEquationDataInput();
+
+            if (_isInputValid)
+            {
+                OutputSolution();
+            }            
         }
 
-        private void ReadInput()
+        private void ReadEquationDataInput()
         {
+            _isInputValid = false;
+
+            double a = (double)nudA.Value;
+            double b = (double)nudB.Value;
+            double accuracy = (double)nudAccuracy.Value;
+
+            if (a >= b)
+            {
+                MessageBox.Show("A can not be greater or equal B", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _isInputValid = true;
             _equation = new Equation((double)nudA.Value, (double)nudB.Value, (double)nudAccuracy.Value);
         }
 
