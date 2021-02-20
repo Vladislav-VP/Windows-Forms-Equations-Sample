3bc9c69cd124c03f728a9df3382680fb4bd013a3
Vladislav Parkhomenko <vladislav.parkhomenko@yandex.ua>
2021-02-20 18:41:26 +0200

    set up equation logic

---
+++ EquationsSample/EquationsSample/Equation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationsSample
+{
+    public class Equation
+    {
+        private double _minValue;
+        private double _maxValue;
+
+        private double _accuracy;
+
+        private double _solution;
+
+        public double CurrentX { get; set; }
+
+        public Equation(double minValue, double maxValue, double accuracy)
+        {
+            _minValue = minValue;
+            _maxValue = maxValue;
+            _accuracy = accuracy;
+        }
+
+        public double GetSolution()
+        {
+            Solve();
+
+            return _solution;
+        }
+
+        public double GetFunctionValue(double x)
+        {
+            double y = Math.Pow(x, 3) - Math.Pow(x, 2) + 3;
+
+            return y;
+        }
+
+        public double GetValueForCurrentX()
+        {
+            return GetFunctionValue(CurrentX);
+        }
+
+        private void Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
