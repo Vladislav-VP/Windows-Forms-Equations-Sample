b4fb7fb51a30122d07698afdfbe2d3357d40bb0f
Vladislav Parkhomenko <vladislav.parkhomenko@yandex.ua>
2021-02-20 13:41:35 +0200

    UI setup

---
+++ EquationsSample/EquationsSample/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace EquationsSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbA = new System.Windows.Forms.Label();
+            this.lbB = new System.Windows.Forms.Label();
+            this.nudA = new System.Windows.Forms.NumericUpDown();
+            this.nudB = new System.Windows.Forms.NumericUpDown();
+            this.nudDelta = new System.Windows.Forms.NumericUpDown();
+            this.lbDelta = new System.Windows.Forms.Label();
+            this.lbInput = new System.Windows.Forms.Label();
+            this.lbOutput = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btEvaluate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDelta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbA
+            // 
+            this.lbA.AutoSize = true;
+            this.lbA.Location = new System.Drawing.Point(7, 57);
+            this.lbA.Name = "lbA";
+            this.lbA.Size = new System.Drawing.Size(20, 20);
+            this.lbA.TabIndex = 0;
+            this.lbA.Text = "A";
+            // 
+            // lbB
+            // 
+            this.lbB.AutoSize = true;
+            this.lbB.Location = new System.Drawing.Point(7, 89);
+            this.lbB.Name = "lbB";
+            this.lbB.Size = new System.Drawing.Size(20, 20);
+            this.lbB.TabIndex = 1;
+            this.lbB.Text = "B";
+            // 
+            // nudA
+            // 
+            this.nudA.DecimalPlaces = 1;
+            this.nudA.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudA.Location = new System.Drawing.Point(85, 55);
+            this.nudA.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.nudA.Name = "nudA";
+            this.nudA.Size = new System.Drawing.Size(69, 26);
+            this.nudA.TabIndex = 2;
+            // 
+            // nudB
+            // 
+            this.nudB.DecimalPlaces = 1;
+            this.nudB.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudB.Location = new System.Drawing.Point(85, 87);
+            this.nudB.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.nudB.Name = "nudB";
+            this.nudB.Size = new System.Drawing.Size(69, 26);
+            this.nudB.TabIndex = 3;
+            // 
+            // nudDelta
+            // 
+            this.nudDelta.DecimalPlaces = 1;
+            this.nudDelta.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudDelta.Location = new System.Drawing.Point(85, 119);
+            this.nudDelta.Name = "nudDelta";
+            this.nudDelta.Size = new System.Drawing.Size(69, 26);
+            this.nudDelta.TabIndex = 4;
+            // 
+            // lbDelta
+            // 
+            this.lbDelta.AutoSize = true;
+            this.lbDelta.Location = new System.Drawing.Point(7, 121);
+            this.lbDelta.Name = "lbDelta";
+            this.lbDelta.Size = new System.Drawing.Size(62, 20);
+            this.lbDelta.TabIndex = 5;
+            this.lbDelta.Text = "Delta X";
+            // 
+            // lbInput
+            // 
+            this.lbInput.AutoSize = true;
+            this.lbInput.Location = new System.Drawing.Point(58, 9);
+            this.lbInput.Name = "lbInput";
+            this.lbInput.Size = new System.Drawing.Size(46, 20);
+            this.lbInput.TabIndex = 6;
+            this.lbInput.Text = "Input";
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.AutoSize = true;
+            this.lbOutput.Location = new System.Drawing.Point(461, 9);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(58, 20);
+            this.lbOutput.TabIndex = 7;
+            this.lbOutput.Text = "Output";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(234, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 204);
+            this.listBox1.TabIndex = 8;
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(494, 55);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(293, 204);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // btEvaluate
+            // 
+            this.btEvaluate.Location = new System.Drawing.Point(641, 294);
+            this.btEvaluate.Name = "btEvaluate";
+            this.btEvaluate.Size = new System.Drawing.Size(146, 102);
+            this.btEvaluate.TabIndex = 10;
+            this.btEvaluate.Text = "Evaluate";
+            this.btEvaluate.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 294);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(572, 102);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 411);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btEvaluate);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.lbInput);
+            this.Controls.Add(this.lbDelta);
+            this.Controls.Add(this.nudDelta);
+            this.Controls.Add(this.nudB);
+            this.Controls.Add(this.nudA);
+            this.Controls.Add(this.lbB);
+            this.Controls.Add(this.lbA);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(820, 450);
+            this.Name = "Form1";
+            this.Text = "Lab 1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDelta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbA;
+        private System.Windows.Forms.Label lbB;
+        private System.Windows.Forms.NumericUpDown nudA;
+        private System.Windows.Forms.NumericUpDown nudB;
+        private System.Windows.Forms.NumericUpDown nudDelta;
+        private System.Windows.Forms.Label lbDelta;
+        private System.Windows.Forms.Label lbInput;
+        private System.Windows.Forms.Label lbOutput;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btEvaluate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
