1b1c0eb73a48d55b03cbc330af88fb0b87aca207
Vladislav Parkhomenko <vladislav.parkhomenko@yandex.ua>
2021-02-20 14:10:11 +0200

    include save button

--- EquationsSample/EquationsSample/Form1.Designer.cs
+++ EquationsSample/EquationsSample/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbA = new System.Windows.Forms.Label();
             this.lbB = new System.Windows.Forms.Label();
@@ -44,17 +44,20 @@
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btEvaluate = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.сохранитьToolStripButton = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.nudA)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudB)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudDelta)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbA
             // 
             this.lbA.AutoSize = true;
-            this.lbA.Location = new System.Drawing.Point(7, 57);
+            this.lbA.Location = new System.Drawing.Point(12, 80);
             this.lbA.Name = "lbA";
             this.lbA.Size = new System.Drawing.Size(20, 20);
             this.lbA.TabIndex = 0;
@@ -63,7 +66,7 @@
             // lbB
             // 
             this.lbB.AutoSize = true;
-            this.lbB.Location = new System.Drawing.Point(7, 89);
+            this.lbB.Location = new System.Drawing.Point(12, 112);
             this.lbB.Name = "lbB";
             this.lbB.Size = new System.Drawing.Size(20, 20);
             this.lbB.TabIndex = 1;
@@ -77,7 +80,7 @@
             0,
             0,
             65536});
-            this.nudA.Location = new System.Drawing.Point(85, 55);
+            this.nudA.Location = new System.Drawing.Point(90, 78);
             this.nudA.Minimum = new decimal(new int[] {
             100,
             0,
@@ -95,7 +98,7 @@
             0,
             0,
             65536});
-            this.nudB.Location = new System.Drawing.Point(85, 87);
+            this.nudB.Location = new System.Drawing.Point(90, 110);
             this.nudB.Minimum = new decimal(new int[] {
             100,
             0,
@@ -113,7 +116,7 @@
             0,
             0,
             65536});
-            this.nudDelta.Location = new System.Drawing.Point(85, 119);
+            this.nudDelta.Location = new System.Drawing.Point(90, 142);
             this.nudDelta.Name = "nudDelta";
             this.nudDelta.Size = new System.Drawing.Size(69, 26);
             this.nudDelta.TabIndex = 4;
@@ -121,7 +124,7 @@
             // lbDelta
             // 
             this.lbDelta.AutoSize = true;
-            this.lbDelta.Location = new System.Drawing.Point(7, 121);
+            this.lbDelta.Location = new System.Drawing.Point(12, 144);
             this.lbDelta.Name = "lbDelta";
             this.lbDelta.Size = new System.Drawing.Size(62, 20);
             this.lbDelta.TabIndex = 5;
@@ -130,7 +133,7 @@
             // lbInput
             // 
             this.lbInput.AutoSize = true;
-            this.lbInput.Location = new System.Drawing.Point(58, 9);
+            this.lbInput.Location = new System.Drawing.Point(63, 32);
             this.lbInput.Name = "lbInput";
             this.lbInput.Size = new System.Drawing.Size(46, 20);
             this.lbInput.TabIndex = 6;
@@ -139,7 +142,7 @@
             // lbOutput
             // 
             this.lbOutput.AutoSize = true;
-            this.lbOutput.Location = new System.Drawing.Point(461, 9);
+            this.lbOutput.Location = new System.Drawing.Point(466, 32);
             this.lbOutput.Name = "lbOutput";
             this.lbOutput.Size = new System.Drawing.Size(58, 20);
             this.lbOutput.TabIndex = 7;
@@ -149,30 +152,30 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(234, 55);
+            this.listBox1.Location = new System.Drawing.Point(239, 78);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(202, 204);
             this.listBox1.TabIndex = 8;
             // 
             // chart1
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chart1.Legends.Add(legend4);
-            this.chart1.Location = new System.Drawing.Point(494, 55);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(499, 78);
             this.chart1.Name = "chart1";
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.chart1.Series.Add(series4);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(293, 204);
             this.chart1.TabIndex = 9;
             this.chart1.Text = "chart1";
             // 
             // btEvaluate
             // 
-            this.btEvaluate.Location = new System.Drawing.Point(641, 294);
+            this.btEvaluate.Location = new System.Drawing.Point(646, 317);
             this.btEvaluate.Name = "btEvaluate";
             this.btEvaluate.Size = new System.Drawing.Size(146, 102);
             this.btEvaluate.TabIndex = 10;
@@ -182,17 +185,37 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(11, 294);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 317);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(572, 102);
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(804, 25);
+            this.toolStrip1.TabIndex = 12;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // сохранитьToolStripButton
+            // 
+            this.сохранитьToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.сохранитьToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("сохранитьToolStripButton.Image")));
+            this.сохранитьToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.сохранитьToolStripButton.Name = "сохранитьToolStripButton";
+            this.сохранитьToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.сохранитьToolStripButton.Text = "&Сохранить";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 411);
+            this.ClientSize = new System.Drawing.Size(804, 431);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btEvaluate);
             this.Controls.Add(this.chart1);
@@ -207,7 +230,7 @@
             this.Controls.Add(this.lbA);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MinimumSize = new System.Drawing.Size(820, 450);
+            this.MinimumSize = new System.Drawing.Size(820, 470);
             this.Name = "Form1";
             this.Text = "Lab 1";
             ((System.ComponentModel.ISupportInitialize)(this.nudA)).EndInit();
@@ -215,6 +238,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.nudDelta)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -234,6 +259,8 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button btEvaluate;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton сохранитьToolStripButton;
     }
 }
 
